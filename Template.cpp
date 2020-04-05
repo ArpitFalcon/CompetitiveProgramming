c50ec3896548904fffb9d9c1c4b0a1a4cb6e4785
ArpitFalcon <arpitfalcon1@gmail.com>
2020-04-05 08:21:57 +0530

    Created a new template for me.

---
+++ Template.cpp
@@ -0,0 +1,45 @@
+/******************************************
+* AUTHOR : ARPIT *
+* NICK : arpitfalcon *
+* INSTITUTION : BIT MESRA *
+* Harwork is the key to success. *
+******************************************/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define vin ll n; cin>>n; vector<ll>x(n); for(ll i=0; i<n; i++) cin>>x[i]
+#define vout for(ll i=0; i<n; i++) cout<<x[i]<<" "
+#define T ll t = 1; cin>>t; while(t--)
+#define ain ll n, k; cin>>n>>k; ll a[n]; for(ll z=0; z<n; z++) cin>>a[z]
+#define aout for(ll i=0; i<n; i++) cout<<a[i]<<" "
+#define sin set<ll>s; ll n; cin>>n; ll tmp; for(ll i=0;i<n;i++){cin>>tmp; s.insert(tmp);}
+#define sout for(auto it=s.begin();it!=s.end();++it) cout<<*it<<" "
+#define pb push_back
+#define mp make_pair
+#define f(i,a,b) for(ll i=a; i<b; i++)
+#define ff first
+#define ss second
+#define endl "\n"
+#define max3(a,b,c) max(a,max(b,c))
+#define min3(a,b,c) min(a,min(b,c))
+#define max4(a,b,c,d) max(a,max3(b,c,d))
+#define min4(a,b,c,d) min(a,min3(b,c,d))
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef long long ll;
+typedef unsigned long long  ull;
+typedef long double ld;
+
+
+int main() {
+    fast;
+    #ifdef _DEBUG
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+	// your code goes here
+    
+    return 0;
+}
