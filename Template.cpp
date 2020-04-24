26fd67daf353939ad1f1271ddfb520e0e86cb9bf
Arpit <arpitfalcon1@gmail.com>
2020-04-24 17:51:13 +0530

    a

--- Template.cpp
+++ Template.cpp
@@ -28,20 +28,21 @@ using namespace std;
 #define min3(a,b,c) min(a,min(b,c))
 #define max4(a,b,c,d) max(a,max3(b,c,d))
 #define min4(a,b,c,d) min(a,min3(b,c,d))
-#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 typedef long long ll;
 typedef unsigned long long  ull;
 typedef long double ld;
 
 
 int main() {
-    fast;
-    #ifdef _DEBUG
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
+	fast;
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
 
 	// your code goes here
-    
-    return 0;
+
+
+	return 0;
 }
