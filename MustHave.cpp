dd127ebb6f3ff69c33ab7cf927ef54d3e79738f4
Arpit <arpitfalcon1@gmail.com>
2020-05-02 11:54:20 +0530

    a

---
+++ MustHave.cpp
@@ -0,0 +1,35 @@
+bool isPrime(int n)
+{
+	// Corner cases
+	if (n <= 1)
+		return false;
+	if (n <= 3)
+		return true;
+
+	if (n % 2 == 0 || n % 3 == 0)
+		return false;
+
+	for (int i = 5; i * i <= n; i = i + 6)
+		if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+
+	return true;
+}
+
+
+bool isPerfectSquare(long double x)
+{
+
+	long double sr = sqrt(x);
+	// If square root is an integer
+	return ((sr - floor(sr)) == 0);
+}
+
+
+bool isPowerOfTwo(int n)
+{
+	if (n == 0)
+		return false;
+
+	return (ceil(log2(n)) == floor(log2(n)));
+}
