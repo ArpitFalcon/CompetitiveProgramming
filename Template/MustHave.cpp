18f205eb5a44d1b797cc9fe5dcd5fee8310e72af
Arpit <arpitfalcon1@gmail.com>
2020-05-13 18:35:22 +0530

    new func

--- Template/MustHave.cpp
+++ Template/MustHave.cpp
@@ -33,3 +33,18 @@ bool isPowerOfTwo(int n)
 
 	return (ceil(log2(n)) == floor(log2(n)));
 }
+
+int countDivisors(int n)
+{
+	int cnt = 0;
+	for (int i = 1; i <= sqrt(n); i++) {
+		if (n % i == 0) {
+			if (n / i == i)
+				cnt++;
+
+			else
+				cnt = cnt + 2;
+		}
+	}
+	return cnt;
+}
