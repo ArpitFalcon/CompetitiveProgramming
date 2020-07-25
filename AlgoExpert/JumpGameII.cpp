1bd3b8ad4eec1b4d1d3bc1a4bf0a4c52e03edc43
Arpit <arpitfalcon1@gmail.com>
2020-07-25 07:54:12 +0530

    logic mojo: 2

--- AlgoExpert/JumpGameII.cpp
+++ AlgoExpert/JumpGameII.cpp
@@ -1,23 +1,19 @@
 void sortColors(vector<int>& nums) {
     int low = 0, traverse = 0, high = nums.size() - 1;
-
     while (traverse <= high) {
-
         if (nums[traverse] == 0) {
             swap(nums[traverse], nums[low]);
             traverse++; low++;
         }
-
         else if (nums[traverse] == 2) {
             swap(nums[traverse], nums[high]);
             high--;
         }
-
         else {
             traverse++;
         }
     }
 }
 
-// LeetCode - https://leetcode.com/problems/sort-colors/
-// Medium
+// LeetCode - https://leetcode.com/problems/jump-game-ii/
+// Hard
