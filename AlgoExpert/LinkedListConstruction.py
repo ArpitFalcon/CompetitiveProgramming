e7ef0f188229c055e4dd427a7162bfa55074aeda
arpitfalcon <arpitfalcon1@gmail.com>
2020-09-03 07:22:26 +0530

    add: class for doubly linked list

---
+++ AlgoExpert/LinkedListConstruction.py
@@ -0,0 +1,20 @@
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def setHead(self, node):
+
+    def setTail(self, node):
+
+    def insertBefore(self, node, nodeToInsert):
+
+    def insertAfter(self, node, nodeToInsert):
+    
+    def insertAtPosition(self, position, nodeToInsert):
+    
+    def removeNodesWithValue(self, value):
+
+    def remove(self, node):
+    
+    def containsNodeWithValue(self, value):
