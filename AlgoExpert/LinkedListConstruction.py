e5027e594b6c6ba4ab757ad1bb45b340de5bd12c
arpitfalcon <arpitfalcon1@gmail.com>
2020-09-03 07:44:34 +0530

    add: comment in remove

--- AlgoExpert/LinkedListConstruction.py
+++ AlgoExpert/LinkedListConstruction.py
@@ -3,12 +3,24 @@ class DoublyLinkedList:
         self.head = None
         self.tail = None
     
-    
+
     def setHead(self, node):
 
     def setTail(self, node):
 
+
     def insertBefore(self, node, nodeToInsert):
+        # If there is only one node 
+        if nodeToInsert == self.head and nodeToInsert == self.tail:
+            return
+        self.remove(nodeToInsert)
+        nodeToInsert.prev = node.prev
+        nodeToInsert.next = node
+        if node.prev is None:
+            self.head = nodeToInsert
+        else:
+            node.prev.next = nodeToInsert
+
 
     def insertAfter(self, node, nodeToInsert):
     
@@ -16,6 +28,8 @@ class DoublyLinkedList:
     
     def removeNodesWithValue(self, value):
         node = self.head
+        # Traverse the linked list
+        # Remove all nodes with that value
         while node is not None:
             nodeToRemove = node
             node = node.next
@@ -24,24 +38,34 @@ class DoublyLinkedList:
 
 
     def remove(self, node):
+        # Check for HEAD
         if node == self.head:
             self.head = self.head.next
+
+        # Check for tail
         if node == self.tail:
             self.tail = self.tail.prev
+        
+        # Remove the bindings (next and prev) properly
         self.removeNodeBindings(node)
     
 
     def containsNodeWithValue(self, value):
         node = self.head
+        # Traverse the linked list to find the node with the value
         while node is not None and node.value != value:
             node = node.next
+        # Return True if node is not none (found).
         return node is not None
 
 
     def removeNodeBindings(self, node):
+        # Check for head node.
         if node.prev is not None:
             node.prev.next = node.next
+        # Check for tail node.
         if node.next is not None:
             node.next.prev = node.prev
+        # Removing the bindings.
         node.prev = None
         node.next = None
