cd14b4817d7a86bb1c69e8869005c66852547159
arpitfalcon <arpitfalcon1@gmail.com>
2020-09-14 18:52:20 +0530

    add: 7th ladder

--- Grind/CF58_A.py
+++ Grind/CF58_A.py
@@ -1,13 +1,17 @@
 check = 'hello'
-
+    
 s = input()
 running = 0
-
+    
 for i in range(len(s)):
     if check[running] == s[i]:
+        # print(str(running) + ' ' + s[i])
         running += 1
-
-if running >= 4:
+    if running >= 5:
+        break
+    
+# print(running)
+if running == 5:
     print('YES')
 else:
     print('NO')
