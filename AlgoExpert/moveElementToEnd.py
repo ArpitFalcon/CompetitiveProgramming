39cae565320bde6ef714a9dd5a9c30d088e84e91
arpitfalcon <arpitfalcon1@gmail.com>
2020-10-01 08:12:27 +0530

    add: algoexpert

--- AlgoExpert/moveElementToEnd.py
+++ AlgoExpert/moveElementToEnd.py
@@ -1,19 +1,21 @@
-def moveElementToEnd(arr, target):
+def moveElementToEnd(array, toMove):
     run = 0
-    high = len(arr) - 1
+    high = len(array) - 1
 
-    while(run < high):
-        if(run == target):
-            arr[run], arr[high] = arr[high], arr[run]
+    while run < high:
+        while run < high and array[high] == toMove:
             high -= 1
-        else:
-            run += 1
+        if array[run] == toMove:
+            array[run], array[high] = array[high], array[run]
+        run += 1
     
-    return arr
+    return array
 
 
 def main():
-    moveElementToEnd([2, 1, 2, 2, 2, 3, 4, 2], 2)
+    print(moveElementToEnd([2, 1, 2, 2, 2, 3, 4, 2], 2))
+    print(moveElementToEnd([3, 24, 1, 56, 2, 4, 2, 1, 7, 8, 1, 7], 1))
+
 
 if __name__ == '__main__':
     main()
