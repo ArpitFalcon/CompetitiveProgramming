2488b96ce4bd18d22415b344e73474ac04e58304
arpitfalcon <arpitfalcon1@gmail.com>
2020-10-09 08:16:39 +0530

    algoexp: NthFib

---
+++ AlgoExpert/NthFibonacci.py
@@ -0,0 +1,30 @@
+# O(2^n) Time | O(n) Space
+def getNthFib(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        return getNthFib(n-1) + getNthFib(n-2)
+
+
+# O(N) Time | O(N) Space
+def getNthFib(n, memoize = {1: 0, 2: 1}):
+    if n in memoize:
+        return memoize[n]
+    else:
+        memoize[n] = getNthFib(n-1, memoize) + getNthFib(n-2, memoize)
+        return memoize[n]
+   
+
+# O(N) Time | O(1) Space
+def getNthFib(n):
+    lastTwo = [0, 1]
+    counter = 3
+    while counter <= n:
+        nextFib = lastTwo[0] + lastTwo[1]
+        lastTwo[0] = lastTwo[1]
+        lastTwo[1] = nextFib
+        counter += 1
+    
+    return lastTwo[1] if n > 1 else lastTwo[0]
