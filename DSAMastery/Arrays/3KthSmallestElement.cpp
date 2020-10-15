728ecbecc68d7971b624bf564d4e10452550fd43
arpitfalcon <arpitfalcon1@gmail.com>
2020-10-15 18:06:49 +0530

    add: kth Largest Element

--- DSAMastery/Arrays/3KthSmallestElement.cpp
+++ DSAMastery/Arrays/3KthSmallestElement.cpp
@@ -7,6 +7,8 @@ void swap(int &a, int &b) {
 	b = temp;
 }
 
+
+// Partition by taking the last element as pivot.
 int partition_last(vector<int> &nums, int l, int r) {
 	int pivot = nums[r], start = l;
 	for (int i = l; i <= r - 1; i++) {
@@ -17,6 +19,15 @@ int partition_last(vector<int> &nums, int l, int r) {
 	}
 	swap(nums[start], nums[r]);
 	return start;
+}
+
+
+// Using this we have expected linear time complexity.
+int partition_random(vector<int> &nums, int l, int r) {
+	int n = r - l + 1;
+	int pivot = rand() % n;
+	swap(nums[l + pivot], nums[r]);
+	return partition_last(nums, l, r);
 }
 
 
