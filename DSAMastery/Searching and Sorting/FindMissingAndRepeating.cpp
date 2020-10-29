4f3b2910cc57072f59b8a9653f3c63005a2777a9
arpitfalcon <arpitfalcon1@gmail.com>
2020-10-29 10:35:29 +0530

    sort: index as values

--- DSAMastery/Searching and Sorting/FindMissingAndRepeating.cpp
+++ DSAMastery/Searching and Sorting/FindMissingAndRepeating.cpp
@@ -1,3 +1,6 @@
+
+// Count Sort
+// O(N) Time | O(N) Space
 vector<int> findTwoElement(int *arr, int n) {
     vector<int> countSort(n + 5, 0);
 
@@ -14,3 +17,28 @@ vector<int> findTwoElement(int *arr, int n) {
 
     return {b, a};
 }
+
+
+// Marking the indexes in the array
+// O(N) Time | O(1) Space
+vector<int> findTwoElement(int *arr, int n) {
+    // b - Repeating Element
+    // a - Missing Element
+    int b, a;
+
+    // Repeating Element
+    for (int i = 0; i < n; i++)
+        if (arr[abs(arr[i]) - 1] > 0)
+            arr[abs(arr[i]) - 1] = -arr[abs(arr[i]) - 1];
+
+        else
+            b = abs(arr[i]);
+
+    // Missing Element
+    for (int i = 0; i < n; i++)
+        if (arr[i] > 0)
+            a = i + 1;
+
+    return {b, a};
+}
+
