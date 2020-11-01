74cf890f2832820730d99f0ffc0337683d888c65
arpitfalcon <arpitfalcon1@gmail.com>
2020-11-01 09:55:23 +0530

    string: KMP and RabinKarp

---
+++ DSAMastery/Strings/PatternSearching.cpp
@@ -0,0 +1,9 @@
+// Pre Process the pattern
+// Rabin Karp - O((n-m+1) * m) in worst case, but works better on average
+// KMP - O(n) in worst case
+// KMP - OP Algorithm
+
+// Pre Process the text
+// Suffix Tree - O(m) in worst case
+
+
