64d6cced05cba4a75a054308b9b489e77928e71b
arpitfalcon <arpitfalcon1@gmail.com>
2020-11-02 10:15:46 +0530

    LL: insert in sorted array

---
+++ DSAMastery/LinkedList/InsertinSortedLL.cpp
@@ -0,0 +1,22 @@
+Node* insertSorted (Node* head, int data) {
+	Node newNode = new Node(data);
+
+	if (head == NULL)
+		return head;
+
+	Node* temp = head;
+
+	if (x < head->data) {
+		newNode -> next = head;
+		return newNode;
+	}
+
+	while (temp->next != NULL and temp->next->data < data) {
+		temp = temp->next;
+	}
+
+	newNode -> next = temp -> next;
+	temp -> next = newNode;
+
+	return head;
+}
