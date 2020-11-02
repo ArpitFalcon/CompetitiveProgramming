4914cc992d36e7b11e13d20ead3ade8107b494ab
arpitfalcon <arpitfalcon1@gmail.com>
2020-11-02 10:04:07 +0530

    LL: insert at position

--- DSAMastery/LinkedList/Insertion.cpp
+++ DSAMastery/LinkedList/Insertion.cpp
@@ -32,3 +32,24 @@ Node* insertAtEnd (Node* head, int x) {
 	curr->next = temp;
 	return head;
 }
+
+
+// Insert at a given position
+Node* insertPos(Node* head, int pos, int data) {
+	if (pos == 1)
+		return insertAtBeginning(head, data);
+
+	int curr = 1;
+	Node* temp = head;
+	Node* newNode = new Node(data);
+
+	while (temp->next != NULL and curr != pos - 1) {
+		temp = temp->next; curr++;
+	}
+
+	if (curr == pos) {
+		newNode -> next = temp -> next;
+		temp -> next = newNode;
+	} else
+		return head;
+}
