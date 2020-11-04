ecaf8bd2596027b48dbb3cf164976ed16dc033d8
arpitfalcon <arpitfalcon1@gmail.com>
2020-11-04 09:55:52 +0530

    CLL: insert at beg

--- DSAMastery/LinkedList/CircularLL.cpp
+++ DSAMastery/LinkedList/CircularLL.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Node
+struct Node {
+	int data;
+	Node* next;
+
+	Node (int x) {
+		data = x;
+		next = NULL;
+	}
+}
+
+
 
 void printLL1 (Node* head) {
 	if (head == NULL) return;
@@ -18,4 +30,25 @@ void printLL2 (Node* head) {
 		cout << temp->data << " ";
 		temp = temp->next;
 	} while (temp != head);
+}
+
+
+// Insert at the beginning in O(1) with no tail pointer
+Node* insertBegin(Node *head, int x) {
+	Node *temp = new Node(x);
+	if (head == NULL) {
+		temp->next = temp;
+		return temp;
+	}
+
+	// Insert at 2nd position
+	temp->next = head->next;
+	head->next = temp;
+
+	// Swap data
+	int temp_data = head->data;
+	head->data = temp->data;
+	temp->data = temp_data;
+
+	return head;
 }
