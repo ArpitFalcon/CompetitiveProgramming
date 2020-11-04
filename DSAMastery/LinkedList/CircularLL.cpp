0c53daa5777bcd3420be1fb10a1f69cea856be6c
arpitfalcon <arpitfalcon1@gmail.com>
2020-11-04 10:31:52 +0530

    CLL: del

--- DSAMastery/LinkedList/CircularLL.cpp
+++ DSAMastery/LinkedList/CircularLL.cpp
@@ -52,3 +52,54 @@ Node* insertBegin(Node *head, int x) {
 
 	return head;
 }
+
+
+// Insert at the end - O(1) without tail pointer
+Node* insertEnd(Node *head, int x) {
+	Node *temp = new Node(x);
+	if (head == NULL) {
+		temp->next = temp;
+		return temp;
+	}
+
+	// Insert at 2nd position
+	temp->next = head->next;
+	head->next = temp;
+
+	// Swap data
+	int temp_data = head->data;
+	head->data = temp->data;
+	temp->data = temp_data;
+
+	return temp;
+}
+
+
+// Delete the head node of the CLL
+Node* deleteHead(Node* head) {
+	if (head == NULL || head->next == NULL)
+		return NULL;
+
+	// Delete the second node and copy it's value in head.
+	head->data = head->next->data;
+	head->next = head->next->next;
+
+	return head;
+}
+
+
+// Delete the kth node from the CLL
+Node* deleteKthNode(Node* head, int k) {
+
+	if (head == NULL)
+		return head;
+
+	if (k == 1)
+		return deleteHead(head);
+
+	Node* curr = head;
+	for (int i = 0; i < k - 2; i++)
+		curr = curr->next;
+
+	curr->next = curr->next->next;
+}
