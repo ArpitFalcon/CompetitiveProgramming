7fd7cede7b186018a646f4a7df54b43f49eefba3
arpitfalcon <arpitfalcon1@gmail.com>
2020-11-06 15:48:33 +0530

    heap: prior queue

---
+++ DSAMastery/Heaps/PriorityQueue.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	priority_queue<int> pq;
+
+	// Insert an element
+	// pq.push(10);
+
+	// Get size
+	// pq.size();
+
+	// Max element
+	// pq.top();
+
+	// Check empty
+	// pq.empty();
+
+	// Pop max element
+	// pq.pop();
+
+	// Min heap in PQ
+	// priority_queue<int, vector<int>, greater<int>> pq;
+}
