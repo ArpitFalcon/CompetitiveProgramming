d149b14d31f093f5ef574e0b6e3d51d9b621b3e7
arpitfalcon <arpitfalcon1@gmail.com>
2020-11-06 10:51:54 +0530

    heaps: insert

--- DSAMastery/Heaps/Implementation.cpp
+++ DSAMastery/Heaps/Implementation.cpp
@@ -1,3 +1,4 @@
+// Complete Binary Tree, Every node must be smaller than it's descendants.
 class MinHeap {
 	int [] arr;
 	int size;
@@ -12,5 +13,17 @@ class MinHeap {
 	int left(int i)	return (2 * i + 1);
 	int right(int i) return (2 * i + 2);
 	int parent(int i) return ((i - 1) / 2);
+
+	void insert(int x) {
+		if (size == capacity) return;
+		size++; arr[size - 1] = x;
+
+		for (int i = size - 1; i != 0 and arr[parent(i)] > arr[i];) {
+			swap(arr[i], arr[parent(i)]);
+			i = parent(i);
+		}
+	}
+
+
 }
 
