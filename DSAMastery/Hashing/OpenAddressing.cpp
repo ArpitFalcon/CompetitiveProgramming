40211931078d8bf1dcdade77a35e10c4812687ee
arpitfalcon <arpitfalcon1@gmail.com>
2020-11-08 10:41:06 +0530

    hash: linear probing

--- DSAMastery/Hashing/OpenAddressing.cpp
+++ DSAMastery/Hashing/OpenAddressing.cpp
@@ -20,3 +20,77 @@
 
 
 // Implementation of Open Addressing
+// Linear Probbing
+struct MyHash {
+	int *arr;
+	int size;
+	int cap;
+
+
+	MyHash(int c) {
+		cap = c;
+		size = 0;
+		for (int i = 0; i < cap; i++)
+			arr[i] = -1;
+	}
+
+
+	int hash(int key) {
+		return key % cap;
+	}
+
+
+	bool search(int key) {
+		int h = hash(key);
+		int i = h;
+
+		while (arr[i] != -1) {
+			if (arr[i] == key)
+				return true;
+			i = (i + 1) % cap;
+			if (i == h)
+				return false;
+		}
+
+		return false;
+	}
+
+
+	bool insert(int key) {
+		if (size == cap)
+			return false;
+
+		int i = hash(key);
+
+		while (arr[i] != -1 and arr[i] != -2 and arr[i] != key)
+			i = (i + 1) % cap;
+
+		if (arr[i] == key)
+			return false;
+		else {
+			arr[i] = key;
+			size++;
+			return true;
+		}
+	}
+
+
+	bool erase (int key)_{
+		if (size == 0)
+			return false;
+
+		int i = hash(key);
+
+		while (arr[i] != -1) {
+			if (arr[i] == key) {
+				arr[i] = -2;
+				return true;
+			}
+
+			i = (i + 1) % cap;
+			if (i == h)
+				return false;
+		}
+		return false;
+	}
+}
