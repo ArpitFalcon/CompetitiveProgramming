be03ccb968aaaec02b1a144547913754b9dd39db
arpitfalcon <arpitfalcon1@gmail.com>
2020-11-09 09:24:59 +0530

    bst: search iterative

--- DSAMastery/BinarySearchTree/Implementation.cpp
+++ DSAMastery/BinarySearchTree/Implementation.cpp
@@ -18,10 +18,25 @@ struct Node {
 bool search(Node* root, int x) {
 	if (root == NULL)
 		return false;
+
 	if (root->key > x)
 		search(root->left, x);
 	else if (root->key < x)
 		search(root->right, x);
 	else if (root->key == x)
 		return true;
+}
+
+
+bool searchIterative(Node *root, int x) {
+	while (root != NULL) {
+		if (root->key == x)
+			return true;
+		else if (root->key < x)
+			root = root->right;
+		else
+			root = root->left;
+	}
+
+	return false;
 }
