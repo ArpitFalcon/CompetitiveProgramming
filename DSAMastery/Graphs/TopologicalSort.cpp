876fc2a703e32f68b7cfe753c1aca94816775ec8
arpitfalcon <arpitfalcon1@gmail.com>
2020-11-10 17:44:06 +0530

    graph: BFS Topological Sort

--- DSAMastery/Graphs/TopologicalSort.cpp
+++ DSAMastery/Graphs/TopologicalSort.cpp
@@ -31,3 +31,36 @@ void topologicalSort(vector<int> adj[], int v) {
 		sortedNums.pop();
 	}
 }
+
+
+//////////////////////////////////////////////////////
+// BFS Implementation of Topological Sort - Kahn's Algorithm
+void topologicalSortBFS(vector<int> adj[], int v) {
+	// Storing the indegree of all vertices
+	int indegree[v];
+	for (int i = 0; i < v; i++) {
+		for (int x : adj[i])
+			indegree[x]++;
+	}
+
+	// Push all the elements with indegree - 0
+	queue<int> sortedNums;
+	for (int i = 0; i < v; i++)
+		if (indegree[i] == 0)
+			sortedNums.push(i);
+
+	while (sortedNums.empty() == false) {
+		// Print the number
+		int u = sortedNums.top();
+		sortedNums.pop();
+		cout << u << " ";
+
+		// Reduct the indegree by one.
+		for (int x : adj[u]) {
+			indegree[x]--;
+			// If zero push into queue.
+			if (indegree[x] == 0)
+				sortedNums.push(x);
+		}
+	}
+}
