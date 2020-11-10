99c35624cdd9cfab497651952314adbba9dccb1d
arpitfalcon <arpitfalcon1@gmail.com>
2020-11-10 17:28:04 +0530

    graph: topological sort

---
+++ DSAMastery/Graphs/TopologicalSort.cpp
@@ -0,0 +1,29 @@
+// The dependant jobs should come later and
+// the jobs on which they are dependant should appear first.
+
+// DFS based solution.
+void DFSRecursive(vector<int> adj[], int u, bool visited[], stack<int> &sortedNums) {
+	visited[u] = true;
+
+	for (int x : adj[u]) {
+		if (visited[x] == false)
+			DFSRecursive(adj, x, visited);
+	}
+
+	sortedNums.push(u);
+}
+
+
+// Primary Function
+void topologicalSort(vector<int> adj[], int v) {
+	stack<int> sortedNums;
+
+	for (int i = 0; i < v; i++)
+		if (visited[u] != true)
+			DFSRecursive(adj, i, visited, sortedNums);
+
+	while (sortedNums.empty() != true) {
+		cout << sortedNums.top() << " ";
+		sortedNums.pop();
+	}
+}
