546d4c855bc723563ba2bc171afb0c803aa33ecf
arpitfalcon <arpitfalcon1@gmail.com>
2020-11-11 11:07:55 +0530

    bit: count set bits

---
+++ DSAMastery/Bit Magic/CountSetBits.cpp
@@ -0,0 +1,22 @@
+// O(number of bits) Time
+void countBits(int n) {
+	int res = 0;
+	while (n > 0) {
+		res += (n & 1);
+		n = n >> 1;
+	}
+
+	return res;
+}
+
+
+// O(set bit count)
+void countBits(int n) {
+	int res = 0;
+	while (n > 0) {
+		n = (n & (n - 1));
+		res++;
+	}
+
+	return res;
+}
