ded974022dc225267b01382d632230716ef83e14
arpitfalcon <arpitfalcon1@gmail.com>
2020-12-01 10:52:18 +0530

    search: rotated array

--- DSAMastery/Searching and Sorting/SearchInARotatedSortedArrayGFG.cpp
+++ DSAMastery/Searching and Sorting/SearchInARotatedSortedArrayGFG.cpp
@@ -4,13 +4,14 @@ int search(int arr[], int n, int x) {
 	while (low <= high) {
 		int mid = low + (high - low) / 2;
 		if (arr[mid] == x) return mid;
-
+		// Left Half is Sorted
 		if (arr[low] < arr[mid]) {
 			if (x >= arr[low] and x < arr[mid])
 				high = mid - 1;
 			else
 				low = mid + 1;
 		}
+		// Right Half is Sorted
 		else {
 			if (x > arr[mid] and x <= arr[high])
 				low = mid + 1;
